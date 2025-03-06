c6022e58e7e64493dfbcaffb0c3c4c84205fd84a
Kaylla <dfing0114@gmail.com>
2025-03-06 16:11:22 +0800

    add: launch screen

--- VideoStreamX/AppDelegate.swift
+++ VideoStreamX/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        accentSetting()
         return true
     }
 
@@ -31,6 +32,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+    private func accentSetting() {
+        let appearance = UINavigationBarAppearance()
+        // 設置導航欄背景顏色
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = .accent
+        // 設置標題文字顏色
+        appearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        // 設置大標題文字顏色 (如果使用大標題)
+        appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        // 應用於所有場景
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+        UINavigationBar.appearance().compactAppearance = appearance
+        // 設置按鈕顏色 (返回按鈕等)
+        UINavigationBar.appearance().tintColor = .white
+    }
 }
 
