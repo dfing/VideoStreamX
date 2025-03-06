894b586b0eb17272fd6890b7cdeeb9caf8bebf03
Kaylla <dfing0114@gmail.com>
2025-03-06 14:09:10 +0800

    Initial Commit

---
+++ VideoStreamX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoStreamX
+//
+//  Created by kaylla on 2025/3/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
